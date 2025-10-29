eb05d828826aa779c5e886142455ced2d95f6b8a
Cole-Bat <cole.batonyi@ucalgary.ca>
2025-10-28 20:05:35 -0600

    feat:binary functionality

--- app/src/main.c
+++ app/src/main.c
@@ -2,12 +2,8 @@
  * main.c
  */
 
-#include <inttypes.h>
 
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/printk.h>
 
 #include "BTN.h"
 #include "LED.h"
@@ -16,14 +12,35 @@
 
 int main(void) {
 
+  int count = 0;
+
   if (0 > BTN_init()) {
     return 0;
   }
-  if (0 > LED_init()) {
+  
+  if (0 > LED_init()){
     return 0;
   }
-
+  
   while(1) {
+
+    LED_set(LED0, count % 2);
+  
+    LED_set(LED1, (count / 2) % 2);
+  
+    LED_set(LED2, (count / 4) % 2);
+
+    LED_set(LED3, (count / 8) % 2);
+
+    
+    if (BTN_check_clear_pressed(BTN0)){
+      count++;
+      printk("Button 0 pressed!\n");
+    }
+
+    if (count == 16)
+      count = 0;
+    
     k_msleep(SLEEP_MS);
   }
 	return 0;
