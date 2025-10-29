358d7397f012ee3b0934a97ad0818a1d221dac6b
Cole-Bat <cole.batonyi@ucalgary.ca>
2025-10-28 22:30:45 -0600

    state machine framework

--- app/src/main.c
+++ app/src/main.c
@@ -2,17 +2,20 @@
  * main.c
  */
 
+#include <inttypes.h>
 
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
 
 #include "BTN.h"
 #include "LED.h"
+#include "my_state_machine.h"
 
 #define SLEEP_MS 1
 
 int main(void) {
-
-  int count = 0;
 
   if (0 > BTN_init()) {
     return 0;
@@ -22,25 +25,15 @@ int main(void) {
     return 0;
   }
   
+  state_machine_init();
+
   while(1) {
 
-    LED_set(LED0, count % 2);
-  
-    LED_set(LED1, (count / 2) % 2);
-  
-    LED_set(LED2, (count / 4) % 2);
-
-    LED_set(LED3, (count / 8) % 2);
-
-    
-    if (BTN_check_clear_pressed(BTN0)){
-      count++;
-      printk("Button 0 pressed!\n");
+    int ret = state_machine_run();
+    if (0 > ret){
+      return 0;
     }
-
-    if (count == 16)
-      count = 0;
-    
+  
     k_msleep(SLEEP_MS);
   }
 	return 0;
